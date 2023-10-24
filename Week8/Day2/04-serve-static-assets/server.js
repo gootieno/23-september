df44b2b6343173c670a59dd52c0f6416d39abf71
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-10-24 14:09:58 -0700

    static asset polya

--- Week8/Day2/04-serve-static-assets/server.js
+++ Week8/Day2/04-serve-static-assets/server.js
@@ -3,6 +3,14 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+
+  // create a route handler for serving the home page (index.html)
+
+
+  // build a route handler for /static/css/application.css
+
+
+  // build a route handler for /static/images/dog.jpg
 });
 
 const port = 5000;
