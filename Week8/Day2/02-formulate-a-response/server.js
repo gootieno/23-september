10cde1b6f09e67a30d44992345ad777163d8ea30
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-10-24 09:58:55 -0700

    servers template

--- Week8/Day2/02-formulate-a-response/server.js
+++ Week8/Day2/02-formulate-a-response/server.js
@@ -1,1 +1,23 @@
 // Your code here
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  // server responses here
+
+  /*
+    get response body
+
+    set statusCode
+
+    set header
+
+    res.write() <-- passing in the response body
+    and res.end()
+    or
+    res.end() <-- passing in the response body
+
+  */
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`server is listening on port ${port}`));
