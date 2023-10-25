86002ff0a675dffa36750b6d92694a680a1660cf
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-10-25 11:01:17 -0700

    sp solutions

--- Week8/Day3/02-async-await/phase-1.js
+++ Week8/Day3/02-async-await/phase-1.js
@@ -2,16 +2,18 @@ function stretch() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("done stretching");
-      resolve();
+      resolve("done stretching");
     }, 1000);
   });
 }
 
 function runOnTreadmill() {
   return new Promise((resolve, reject) => {
+    return reject("not running");
+
     setTimeout(() => {
       console.log("done running on treadmill");
-      resolve();
+      resolve("done running on treadmill");
     }, 500);
   });
 }
@@ -20,20 +22,35 @@ function liftWeights() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("done lifting weights");
-      resolve();
+      resolve("done lifting weights");
     }, 2000);
   });
 }
 
 // refactor this function to handle Promises using async/await instead of
-  // .then and .catch
-function workout() {
-  stretch()
-    .then(runOnTreadmill)
-    .then(liftWeights)
-    .then(() => console.log("done working out"))
-    .catch((err) => console.log(err));
+// .then and .catch
+async function workout() {
+  // stretch()
+  //   .then(runOnTreadmill)
+  //   .then(liftWeights)
+  //   .then(() => console.log("done working out"))
+  //   .catch((err) => console.log(err));
+  try {
+    await stretch();
+    await runOnTreadmill();
+    await liftWeights();
+    console.log("done working out");
+  } catch (err) {
+    console.log(err);
+  }
 }
+const allPromisesWorkout = () => {
+  Promise.all([stretch(), runOnTreadmill(), liftWeights()])
+    .then((promises) => console.log("promises ", promises))
+    .catch((error) => console.log(error));
+};
+
+allPromisesWorkout();
 
 /* ============================ TEST YOUR CODE ============================
 
@@ -41,10 +58,9 @@ Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
-workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// workout();
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
--- Week8/Day3/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/03-fetch-from-server/assets/js/your-code.js
@@ -1,19 +1,27 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  const url = "/dogs";
+  // Use the URLSearchParams API to format your body as shown below
+
+  const options = {
+    method: "GET",
+  };
+  //   return fetch(url, options);
+  return fetch(url);
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
 }
 
 export function postNewDog() {
-    // Your code here
+  // Your code here
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
 }
