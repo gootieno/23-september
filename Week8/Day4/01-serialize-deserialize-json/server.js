0a1dc83a088ccdad3e922c3014ec88601e3613a5
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-10-26 10:09:19 -0700

    json and endpoints

--- Week8/Day4/01-serialize-deserialize-json/server.js
+++ Week8/Day4/01-serialize-deserialize-json/server.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
@@ -10,27 +10,37 @@ const server = http.createServer((req, res) => {
 
   req.on("end", () => {
     // Parse the body of the request as JSON if Content-Type header is
-      // application/json
+    // application/json
     // Parse the body of the request as x-www-form-urlencoded if Content-Type
-      // header is x-www-form-urlencoded
+    // header is application/x-www-form-urlencoded
+
     if (reqBody) {
-      req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
-        .reduce((acc, [key, value]) => {
-          acc[key] = value;
-          return acc;
-        }, {});
+      // if else to and the content type. Parse accordingly
+      if (req.headers["content-type"] === "application/json") {
+        req.body = JSON.parse(reqBody);
+      } else {
+        req.body = reqBody
+          .split("&")
+          .map((keyValuePair) => keyValuePair.split("="))
+          .map(([key, value]) => [key, value.replace(/\+/g, " ")])
+          .map(([key, value]) => [key, decodeURIComponent(value)])
+          .reduce((acc, [key, value]) => {
+            acc[key] = value;
+            return acc;
+          }, {});
+      }
 
       // Log the body of the request to the terminal
       console.log(req.body);
     }
 
     const resBody = {
-      "Hello": "World!"
+      Hello: "World!",
     };
+
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    return res.end(JSON.stringify(resBody));
 
     // Return the `resBody` object as JSON in the body of the response
   });
@@ -38,4 +48,4 @@ const server = http.createServer((req, res) => {
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
---
+++ Week8/Day4/02-creating-api-docs/endpoints.md
@@ -0,0 +1,22 @@
+- Get all the posts
+- Create a new post
+- Edit a post
+- Create a new user
+- Get the comments for a post
+- Create a comment for a post
+- Edit a comment for a post
+- Delete a comment for a post
+- Add a like for a post
+- Remove a like for a post
+- Get all the posts of a user
+- Submit a search on posts
+
+
+```js
+const person = {
+    username: 'kaido',
+    password: 'kaido1',
+    email:'kaido@gmail.com'
+}
+
+```
