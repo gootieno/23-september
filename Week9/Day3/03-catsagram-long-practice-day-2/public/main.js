2d77ba5fcfa594b970d949aa95626a2591163177
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-11-01 12:58:37 -0700

    catsagram

--- Week9/Day3/03-catsagram-long-practice-day-2/public/main.js
+++ Week9/Day3/03-catsagram-long-practice-day-2/public/main.js
@@ -1,30 +1,34 @@
-export const createMainContent = () => {
-    // Create h1
-    const h1 = document.createElement("h1");
-    h1.innerText = "Catstagram";
+export const createMainContent = async () => {
+  // Create h1
+  const h1 = document.createElement("h1");
+  h1.innerText = "Catstagram";
 
-    // Create img
-    const img = document.createElement("img");
-    img.style.margin = "20px";
-    img.style.maxWidth = "750px";
+  // Create img
+  const img = document.createElement("img");
+  img.setAttribute("class", "cat-image");
+  img.style.margin = "20px";
+  img.style.maxWidth = "750px";
 
-    const container = document.querySelector(".container");
-    container.appendChild(h1);
-    container.appendChild(img);
+  const container = document.querySelector(".container");
+  container.appendChild(h1);
+  container.appendChild(img);
 
-    fetchImage();
+  const kittenUrl = await fetchImage();
+  console.log("kitten url ", kittenUrl);
+  img.src = kittenUrl;
 };
 
 const fetchImage = async () => {
-    // Fetch image from API and set img url
-    try {
-        const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
-        // Converts to JSON
-        const kittenData = await kittenResponse.json();
-        // console.log(kittenData);
-        const kittenImg = document.querySelector("img");
-        kittenImg.src = kittenData[0].url;
-    } catch (e) {
-        console.log("Failed to fetch image", e);
-    }
+  // Fetch image from API and set img url
+  try {
+    const kittenResponse = await fetch(
+      "https://api.thecatapi.com/v1/images/search?size=small"
+    );
+    // Converts to JSON
+    const kittenData = await kittenResponse.json();
+    console.log(kittenData);
+    return kittenData[0].url;
+  } catch (e) {
+    console.log("Failed to fetch image", e);
+  }
 };
